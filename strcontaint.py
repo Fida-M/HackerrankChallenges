3f0ebb349a9ce314a68da25fb49831d1319a23b9
Fida-M <155111305+Fida-M@users.noreply.github.com>
2024-03-10 17:02:06 +0100

    Add files via upload

---
+++ strcontaint.py
@@ -0,0 +1,11 @@
+def check_string(s):
+    print(any(char.isalnum() for char in s))
+    print(any(char.isalpha() for char in s))
+    print(any(char.isdigit() for char in s))
+    print(any(char.islower() for char in s))
+    print(any(char.isupper() for char in s))
+
+# Sample Input
+input_string = "qA"
+# Function Call
+check_string(input_string)
